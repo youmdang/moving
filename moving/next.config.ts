09317c57172b5c5ff4352513bbeb352b91cb2f26
jangyonghan <wkddydgks1@gmail.com>
2024-11-16 21:34:54 +0900

    feat : 수정

--- moving/next.config.ts
+++ moving/next.config.ts
@@ -1,8 +1,23 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack: (config) => {
+    // @svgr/webpack 설정 추가
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    // Path Alias 설정 추가
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/icons': path.resolve(__dirname, 'public/icons'), // public/icons alias 추가
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- moving/src/pages/_app.tsx
+++ moving/src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import Navbar from "@/components/shared/Navbar";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Navbar from '@/components/shared/Navbar';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-  <Navbar/>
-  <Component {...pageProps} />
-  </>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
   );
 }
