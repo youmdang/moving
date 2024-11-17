05063ef0a612f2e239af37017abad5d91e3cc1df
jangyonghan <wkddydgks1@gmail.com>
2024-11-17 15:28:32 +0900

    feat#7 : clsx 삭제

--- moving/tailwind.config.ts
+++ moving/tailwind.config.ts
@@ -11,6 +11,11 @@ export default {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        blue : "#2D73F3",
+        orange : "#F29B2E",
+        yellow : "#F2B42E", 
+        black : "#282C33",
+        gray : "#3E4248",
       },
     },
   },
