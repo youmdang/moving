c134f4b989f4b5ad38d924f6c52382a2785ceee6
yamdang <youm4000@naver.com>
2024-11-20 16:26:15 +0900

    Design: 모달 연관작품 컴포넌트 구현

--- moving/src/components/detail/components/RelatedWorksTab.tsx
+++ moving/src/components/detail/components/RelatedWorksTab.tsx
@@ -1,3 +1,151 @@
+import Image from 'next/image';
+
 export default function RelatedWorksTab() {
-  return <div className="px-20 text-white">연관작품 컴포넌트 영역</div>;
+  return (
+    <div className="grid grid-cols-[repeat(6,1fr)] gap-x-6 gap-y-9 px-20 pb-10 text-white">
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-2 flex-shrink-0">
+          <Image
+            src="/images/seriesTestImage.png"
+            width={130}
+            height={178}
+            alt="포스터 이미지"
+          />
+        </div>
+        <h4 className="line-clamp-1 text-base font-semibold text-white">
+          스파이더맨: 어크로스 더 유니버스
+        </h4>
+      </div>
+    </div>
+  );
 }
