3f551f6d4e1e0315c9eacb9bc79939871db91d98
yamdang <youm4000@naver.com>
2024-11-29 01:33:50 +0900

    feat: 모달 예고편 시청 페이지 추가

--- moving/src/lib/apis/modal/api.ts
+++ moving/src/lib/apis/modal/api.ts
@@ -42,6 +42,7 @@ export const fetchSeriesData = async ({
   }
 };
 
+// 출연진 데이터 가져오기
 export const fetchCreditData = async ({
   movieId = 912649,
 }: {
@@ -51,6 +52,56 @@ export const fetchCreditData = async ({
     const response = await axiosInstance.get(`movie/${movieId}/credits`);
     return response.data;
   } catch (error) {
-    console.log('시리즈 데이터 오류', error);
+    console.log('출연진 데이터 오류', error);
+  }
+};
+
+// 리뷰 데이터 가져오기
+export const fetchReviewData = async ({
+  movieId = 912649,
+  language = 'ko-KR',
+}: {
+  movieId: number;
+  language: string | null;
+}) => {
+  try {
+    const response = await axiosInstance.get(`movie/${movieId}/reviews`, {
+      params: {
+        language,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log('리뷰 데이터 오류', error);
+  }
+};
+
+// 관련작품 데이터 가져오기
+export const fetchRecommendationData = async ({
+  movieId = 912649,
+}: {
+  movieId: number;
+}) => {
+  try {
+    const response = await axiosInstance.get(
+      `movie/${movieId}/recommendations`
+    );
+    return response.data;
+  } catch (error) {
+    console.log('관련작품 데이터 오류', error);
+  }
+};
+
+// 예고편 데이터 가져오기
+export const fetchTrailerData = async ({
+  movieId = 1159311,
+}: {
+  movieId: number;
+}) => {
+  try {
+    const response = await axiosInstance.get(`movie/${movieId}/videos`);
+    return response.data;
+  } catch (error) {
+    console.log('예고편 데이터 오류', error);
   }
 };
